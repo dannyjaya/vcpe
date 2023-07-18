10c5b0310a857f8e499c79cba83a3fe6f97b6498
dannyjaya <58035764+dannyjaya@users.noreply.github.com>
2023-07-18 10:48:10 +0700

    Add files via upload

---
+++ convert-b64-helper.js
@@ -0,0 +1,10 @@
+function getBase64(file) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+        console.log(reader.result);
+    };
+    reader.onerror = function (error) {
+        KDF.showError('Error: ', error);
+    };
+}
